902a9b4335ef6577b6df99c09196a5ec04a55db9
ashwath007 <vignesh@hawky.ai>
2025-06-13 00:14:35 +0530

    app route / google tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -70,7 +70,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        {/* Google Tag Manager */}
+        {/* Google Tag Manager - Original */}
         <Script id="google-tag-manager" strategy="afterInteractive">
           {`
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
@@ -80,7 +80,19 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             })(window,document,'script','dataLayer','GTM-5WB2NFZC');
           `}
         </Script>
-        {/* End Google Tag Manager */}
+        {/* End Google Tag Manager - Original */}
+        
+        {/* Google Tag Manager - New */}
+        <Script id="google-tag-manager-new" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PH7NXDN2');
+          `}
+        </Script>
+        {/* End Google Tag Manager - New */}
 
         {/* Google Analytics */}
         <Script
@@ -142,7 +154,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         />
       </head>
       <body className={`${sourceSans.className} ${sourceSans.variable} ${raleway.variable}`}>
-        {/* Google Tag Manager (noscript) */}
+        {/* Google Tag Manager (noscript) - Original */}
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-5WB2NFZC"
@@ -150,6 +162,17 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             width="0"
             style={{ display: "none", visibility: "hidden" }}
           />
+        </noscript>
+        {/* End Google Tag Manager (noscript) - Original */}
+        
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PH7NXDN2"
+            height="0" 
+            width="0" 
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
         </noscript>
         {/* End Google Tag Manager (noscript) */}
 
--- app/privacy-policy/layout.tsx
+++ app/privacy-policy/layout.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from "next"
+import Script from "next/script"
 
 export const metadata: Metadata = {
   title: "Privacy Policy | Hawky.ai",
@@ -26,5 +27,18 @@ export default function PrivacyPolicyLayout({
 }: {
   children: React.ReactNode
 }) {
-  return children
+  return (
+    <>
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-PH7NXDN2');
+        `}
+      </Script>
+      {children}
+    </>
+  )
 } 
